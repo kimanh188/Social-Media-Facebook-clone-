a5d027e88cd6a3c7ad4e1ed637e8e51639fef992
Kim <kimanh188.de@gmail.com>
2023-04-27 09:00:16 +0200

    Dark/Light Button

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,26 @@
 var settingsmenu = document.querySelector(".settings-menu");
+var darkBtn = document.getElementById("dark-btn");
 
 function settingsMenuToggle() {
   settingsmenu.classList.toggle("settings-menu-height");
 }
+
+darkBtn.onclick = function () {
+  darkBtn.classList.toggle("dark-btn-on");
+  document.body.classList.toggle("dark-theme");
+};
+
+/* if theme is light, button dark/light should be off and dark-theme should be off. And vice versa (else if). Else: create light theme when user visit website the first time : */
+if (localStorage.getItem("theme") == "light") {
+  darkBtn.classList.remove("dark-btn-on");
+  document.body.classList.remove("dark-theme");
+} else if (localStorage.getItem("theme") == "dark") {
+  darkBtn.classList.add("dark-btn-on");
+  document.body.classList.add("dark-theme");
+} else {
+  localStorage.setItem("theme", "light");
+}
+
+/* setting local storage with name of "theme" and value "light/dark" */
+/* localStorage.setItem("theme", "light");
+localStorage.getItem("theme"); */
